f1f60030afdc27cb281cb66db3a2c9193e4f318e
DerFetzer <kontakt@der-fetzer.de>
2025-04-18 23:58:52 +0200

    Minimal API integration test

---
+++ tests/container/mod.rs
@@ -0,0 +1,70 @@
+use std::time::Duration;
+
+use hab_rs::rest_api::configuration::Configuration;
+use testcontainers::{
+    ContainerAsync, GenericImage, ImageExt,
+    core::{ExecCommand, IntoContainerPort, WaitFor},
+    runners::AsyncRunner,
+};
+
+pub struct OpenhabContainer {
+    container: ContainerAsync<GenericImage>,
+    api_token: String,
+}
+
+impl OpenhabContainer {
+    pub async fn new() -> OpenhabContainer {
+        let container = GenericImage::new("openhab/openhab", "4.3.4-alpine")
+            .with_wait_for(WaitFor::Duration {
+                length: Duration::from_secs(40),
+            })
+            .with_mapped_port(8080, 8080.tcp())
+            .with_startup_timeout(Duration::from_secs(120))
+            .start()
+            .await
+            .expect("Could not start openhab container");
+        println!("Container started");
+
+        // create admin user and API token
+        let mut exec_result = container
+        .exec(ExecCommand::new(["sh", "-c", "echo \"openhab:users add admin admin administrator;openhab:users addApiToken admin token authentication\" | /openhab/runtime/bin/client -u openhab -p habopen -b"]))
+        .await
+        .expect("Could not prepare container");
+        println!("Executed command");
+
+        let stdout = String::from_utf8(
+            exec_result
+                .stdout_to_vec()
+                .await
+                .expect("Could not get command output"),
+        )
+        .unwrap();
+        let stderr = String::from_utf8(
+            exec_result
+                .stderr_to_vec()
+                .await
+                .expect("Could not get command output"),
+        )
+        .unwrap();
+        println!("stdout: {stdout}");
+        println!("stderr: {stderr}");
+
+        let api_token = stdout
+            .lines()
+            .last()
+            .expect("No command output")
+            .to_string();
+        OpenhabContainer {
+            container,
+            api_token,
+        }
+    }
+
+    pub fn get_api_configuration(&self) -> Configuration {
+        Configuration {
+            base_path: "http://localhost:8080/rest".to_string(),
+            basic_auth: Some((self.api_token.clone(), Some(String::new()))),
+            ..Configuration::default()
+        }
+    }
+}
---
+++ tests/integration_test.rs
@@ -0,0 +1,45 @@
+use std::{error::Error, sync::Arc};
+
+use container::OpenhabContainer;
+use hab_rs::{
+    item::Item,
+    rest_api::{Api, ApiClient},
+    rest_api_models::GroupItemDto,
+};
+
+mod container;
+
+#[tokio::test]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let openhab_container = OpenhabContainer::new().await;
+    let config = openhab_container.get_api_configuration();
+    let api = ApiClient::new(Arc::new(config));
+    let items_api = api.items_api();
+    let created_item = items_api
+        .add_or_update_item_in_registry(
+            "test_string",
+            GroupItemDto {
+                name: Some("test_string".to_string()),
+                r#type: Some("String".to_string()),
+                ..Default::default()
+            },
+            None,
+        )
+        .await?;
+    println!("Created item: {created_item:#?}");
+    let item = items_api
+        .get_item_by_name("test_string", None, None, None)
+        .await?;
+    println!("Got item: {item:#?}");
+
+    let string_item = Item("test_string".to_string());
+
+    let null_state = string_item.state(items_api).await?;
+    assert_eq!(null_state, "NULL");
+
+    string_item.post_update(items_api, "Init Value").await?;
+    let init_state = string_item.state(items_api).await?;
+    assert_eq!(init_state, "Init Value");
+
+    Ok(())
+}
